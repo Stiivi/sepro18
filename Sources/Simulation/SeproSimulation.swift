4e7b45ba828855b4637f4c2ad128b13a376c7ffa
Stefan Urbanek <stefan@agentfarms.net>
2018-10-17 09:53:28 +0200

    Added few debugging print statements to simulator

--- Sources/Simulation/SeproSimulation.swift
+++ Sources/Simulation/SeproSimulation.swift
@@ -21,23 +21,29 @@ public class SeproSimulation: IterativeSimulation {
         self.container = container
     }
 
+    var stepCount: Int = 0
+
     public func step() -> StepResult<SeproSignal> {
         // var notifications = Set<Symbol>()
         // var traps = Set<Symbol>()
         // var halts: Bool = false
 
+        stepCount += 1
+        debugPrint("STEP \(stepCount)")
+
         // Unary actuators
         // ---------------
         // FIXME: Those inner conditions are not 100% right - what if an object
         // has been changed in a way that it has to be considered?
-        for actuator in model.unaryActuators.values {
-
+        for (label, actuator) in model.unaryActuators {
             for oid in container.select(actuator.selector) {
                 if !container.matches(oid, selector: actuator.selector) {
                     // The object has been modified through some of the rules,
                     // we skip it
+                    debugPrint("skip")
                     continue
                 }
+                debugPrint("ACT \(label):\(oid)")
                 container.update(oid, with: actuator.transitions)
             }
         }
@@ -45,7 +51,7 @@ public class SeproSimulation: IterativeSimulation {
         // Binary actuators
         // ----------------
 
-        for actuator in model.binaryActuators.values {
+        for (label, actuator) in model.binaryActuators {
             let leftOnes = container.select(actuator.leftSelector)
             let rightOnes = container.select(actuator.rightSelector)
 
@@ -60,6 +66,8 @@ public class SeproSimulation: IterativeSimulation {
                         // The right has been modified, we skip it
                         continue
                     }
+
+                    debugPrint("REACT \(label): \(left) ON \(right)")
                     container.update(left,
                                      with: actuator.leftTransitions,
                                      other: right)
